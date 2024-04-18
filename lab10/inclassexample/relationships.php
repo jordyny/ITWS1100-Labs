9bda5175d3eb900e1d376c45683eee866985c8c9
jordyny <jordyn.young2234@gmail.com>
2024-04-18 12:37:32 -0400

    update

--- lab10/inclassexample/relationships.php
+++ lab10/inclassexample/relationships.php
@@ -1,17 +1,49 @@
-<?php 
-  include('includes/init.inc.php'); // include the DOCTYPE and opening tags
-  include('includes/functions.inc.php'); // functions
-?>
-<title>PHP &amp; MySQL - ITWS</title>   
+<?php
+include('includes/init.inc.php');
+include('includes/functions.inc.php');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-<?php include('includes/head.inc.php'); ?>
+include('includes/head.inc.php');
+?>
 
 <h1>PHP &amp; MySQL</h1>
-      
+
 <?php include('includes/menubody.inc.php'); ?>
 
-<p>Build the movie forms and output here.</p>
+<?php
+// Database connection
+$dbOk = false;
+@$db = new mysqli('localhost', 'root', 'j0000rdyn!', 'iit');
 
-<?php include('includes/foot.inc.php'); 
-  // footer info and closing tags
+if ($db->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+} else {
+    $dbOk = true;
+}
+
+// Display movies and their corresponding actors
+echo '<h3>Movies and Actors</h3>';
+echo '<table id="relationshipsTable">';
+if ($dbOk) {
+    $query = 'SELECT * FROM actor_movie_relationship';
+    $result = $db->query($query);
+    $numRecords = $result->num_rows;
+    echo '<tr><th>Movie:</th><th>Actor:</th></tr>';
+    while ($record = $result->fetch_assoc()) {
+        echo '<tr><td>';
+        echo htmlspecialchars($record['title']);
+        echo '</td><td>';
+        echo htmlspecialchars($record['full_name']);
+        echo '</td></tr>';
+    }
+    $result->free();
+
+    // Finally, let's close the database
+    $db->close();
+}
+echo '</table>';
 ?>
+
+<?php include('includes/foot.inc.php'); ?>
