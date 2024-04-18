08a5f7728b03549e030d5150695f6c067c3857fe
jordyny <jordyn.young2234@gmail.com>
2024-04-18 12:00:02 -0400

    update js

--- lab10/inclassexample/resources/iit.js
+++ lab10/inclassexample/resources/iit.js
@@ -1,79 +1,66 @@
 function validate(formObj) {
-  
-  if (formObj.firstNames.value == "") {
-    alert("Please enter a first name");
-    formObj.firstNames.focus();
-    return false;
-  }
-  
-  if (formObj.lastName.value == "") {
-    alert("Please enter a last name");
-    formObj.lastName.focus();
-    return false;
-  }
-  
-  if (formObj.dob.value == "") {
-    alert("Please enter a date of birth");
-    formObj.dob.focus();
-    return false;
-  }
+  if (formObj.firstNames && formObj.lastNames && formObj.dob) { // Actor form validation
+    if (formObj.firstNames.value === "") {
+      alert("Please enter a first name");
+      formObj.firstNames.focus();
+      return false;
+    }
 
-  if (formObj.title.value == "") {
-    alert("Please enter a title");
-    formObj.title.focus();
-    return false;
-  }
+    if (formObj.lastName.value === "") {
+      alert("Please enter a last name");
+      formObj.lastName.focus();
+      return false;
+    }
 
-    
+    if (formObj.dob.value === "") {
+      alert("Please enter a date of birth");
+      formObj.dob.focus();
+      return false;
+    }
+  } else if (formObj.title && formObj.year) { 
+    if (formObj.title.value === "") {
+      alert("Please enter a movie title");
+      formObj.title.focus();
+      return false;
+    }
+
+    if (formObj.year.value === "") {
+      alert("Please enter a release year");
+      formObj.year.focus();
+      return false;
+    }
+  }
   return true;
 }
 
-//validate for movies
-
-
 $(document).ready(function() {
-  
-  // focus the name field on first load of the page
-  $("#firstNames").focus();
-     
+  // Focus the appropriate field on first load of the page
+  if ($("#firstNames").length > 0) { // If actor form exists
+    $("#firstNames").focus();
+  } else if ($("#title").length > 0) { // If movie form exists
+    $("#title").focus();
+  }
+
   $(".deleteActor").click(function() {
     if(confirm("Remove actor? (This action cannot be undone.)")) {
-      
-      // get the id of the clicked element's row
       var curId = $(this).closest("tr").attr("id");
-      // Extract the db id of the actor from the dom id of the clicked element
-      var actorId = curId.substr(curId.indexOf("-")+1);
-      // Build the data to send. 
+      var actorId = curId.substr(curId.indexOf("-") + 1);
       var postData = "id=" + actorId;
-      // we could also format this as json ... jQuery will (by default) 
-      // convert it into a query string anyway, e.g. 
-      // var postData = { "id" : actorId };
-      
+
       $.ajax({
         type: "post",
         url: "actor-delete.php",
         dataType: "json",
         data: postData,
-        success: function(responseData, status){
+        success: function(responseData, status) {
           if (responseData.errors) {
             alert(responseData.errno + " " + responseData.error);
           } else {
-            // Uncomment the following line to see the repsonse message from the server
-            // alert(responseData.message);
-            
-            // remove the table row in which the image was clicked
             $("#" + curId).closest("tr").remove();
-            
-            // if a php generated message box is up, hide it:
             $(".messages").hide();
-            
-            // populate the js message box and show it:
             $("#jsMessages").html("<h4>Actor deleted</h4>").show();
-            
-            // re-zebra the table
-            $("#actorTable tr").each(function(i){
+            $("#actorTable tr").each(function(i) {
               if (i % 2 == 0) {
-                // we must compensate for the header row...
                 $(this).addClass("odd"); 
               } else {
                 $(this).removeClass("odd");
@@ -82,78 +69,43 @@ $(document).ready(function() {
           }
         },
         error: function(msg) {
-          // there was a problem
           alert(msg.status + " " + msg.statusText);
         }
       });
-      
     }
   });
 
+  $(".deleteMovie").click(function() {
+    if(confirm("Remove movie? (This action cannot be undone.)")) {
+      var curId = $(this).closest("tr").attr("id");
+      var movieId = curId.substr(curId.indexOf("-") + 1);
+      var postData = "id=" + movieId;
 
-  
+      $.ajax({
+        type: "post",
+        url: "movie-delete.php",
+        dataType: "json",
+        data: postData,
+        success: function(responseData, status) {
+          if (responseData.errors) {
+            alert(responseData.errno + " " + responseData.error);
+          } else {
+            $("#" + curId).closest("tr").remove();
+            $(".messages").hide();
+            $("#jsMessages").html("<h4>Movie deleted</h4>").show();
+            $("#movieTable tr").each(function(i) {
+              if (i % 2 == 0) {
+                $(this).addClass("odd"); 
+              } else {
+                $(this).removeClass("odd");
+              }
+            });
+          }
+        },
+        error: function(msg) {
+          alert(msg.status + " " + msg.statusText);
+        }
+      });
+    }
+  });
 });
-
-// $(document).ready(function() {
-  
-//   // focus the name field on first load of the page
-//   $("#firstNames").focus();
-//   $(".deleteMovie").click(function() {
-//     if(confirm("Remove movie? (This action cannot be undone.)")) {
-      
-//       // get the id of the clicked element's row
-//       var curId = $(this).closest("tr").attr("id");
-//       // Extract the db id of the movie from the dom id of the clicked element
-//       var movieId = curId.substr(curId.indexOf("-")+1);
-//       // Build the data to send. 
-//       var postData = "id=" + movieId;
-//       // we could also format this as json ... jQuery will (by default) 
-//       // convert it into a query string anyway, e.g. 
-//       // var postData = { "id" : movieId };
-      
-//       $.ajax({
-//         type: "post",
-//         url: "movie-delete.php",
-//         dataType: "json",
-//         data: postData,
-//         success: function(responseData, status){
-//           if (responseData.errors) {
-//             alert(responseData.errno + " " + responseData.error);
-//           } else {
-//             // Uncomment the following line to see the repsonse message from the server
-//             // alert(responseData.message);
-            
-//             // remove the table row in which the image was clicked
-//             $("#" + curId).closest("tr").remove();
-            
-//             // if a php generated message box is up, hide it:
-//             $(".messages").hide();
-            
-//             // populate the js message box and show it:
-//             $("#jsMessages").html("<h4>Movie deleted</h4>").show();
-            
-//             // re-zebra the table
-//             $("#actorTable tr").each(function(i){
-//               if (i % 2 == 0) {
-//                 // we must compensate for the header row...
-//                 $(this).addClass("odd"); 
-//               } else {
-//                 $(this).removeClass("odd");
-//               }
-//             });
-//           }
-//         },
-//         error: function(msg) {
-//           // there was a problem
-//           alert(msg.status + " " + msg.statusText);
-//         }
-//       });
-      
-//     }
-//   });
-
-
-  
-// });
-
-
